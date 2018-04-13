1b1d2167f2e676e6f679eb64366448bc2ea87608
Daniel Wiberg <daniel@skillster.se>
2018-04-13 13:17:04 +0200

    Started to implement a simple command prompt.

--- src/cmd.rs
+++ src/cmd.rs
@@ -1,20 +1,57 @@
 
-pub struct Var {
+use std::rc::{Rc};
+use std::cell::{RefCell};
+use std::collections::HashMap;
 
+#[derive(Clone, Debug)]
+pub struct Var(Rc<RefCell<i32>>);
+
+impl Var {
+   fn new() -> Var {
+      Var(Rc::new(RefCell::new(0)))
+   }
+
+   pub fn set(&self, value: i32) {
+      *self.0.borrow_mut() = value;
+   }
+
+   pub fn get(&self) -> i32 {
+      *self.0.borrow()
+   }
 }
 
 pub struct Function {
 
 }
 
-pub fn register_var(_name: String) -> Result<(), String> {
-   Ok(())
+
+pub struct Cmd {
+   vars: RefCell<HashMap<String, Var>>,
 }
 
-pub fn register_func(_name: String) -> Result<(), String> {
-   Ok(())
-}
+impl Cmd {
+   pub fn new() -> Cmd {
+      Cmd {
+         vars: RefCell::new(HashMap::new()),
+      }
+   }
 
-pub fn exec(_line: String) -> Result<(), String> {
-   Ok(())
+   pub fn register_var(&self, name: String) -> Result<(), String> {
+      let mut vars = self.vars.borrow_mut();
+
+      if vars.contains_key(&name) {
+         return Err("Variable already pressent".to_string());
+      }
+
+      vars.insert(name, Var::new());
+      Ok(())
+   }
+
+   pub fn register_func(&self, _name: String) -> Result<(), String> {
+      Ok(())
+   }
+
+   pub fn exec(&self, _line: String) -> Result<(), String> {
+      Ok(())
+   }
 }
